db7c3cd196e0df86739f287ce3b9c9bc1f5e90a3
mohammad <mohammadpirasteh1382@gmail.com>
2024-07-20 14:06:10 -0700

    last

---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'tj3tt5f2',
+    dataset: 'production'
+  }
+})
---
+++ sanity/sanity.config.ts
@@ -0,0 +1,17 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'new-p1',
+
+  projectId: 'tj3tt5f2',
+  dataset: 'production',
+
+  plugins: [structureTool(),visionTool()],
+  schema: {
+    types: schemaTypes,
+  },
+})
